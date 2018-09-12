9a3f3f094b4888f82f1980a1e38d13b096ee8dae
Masaroto <ra103572@uem.br>
2018-09-12 16:45:35 -0300

    main pronta

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 double soma(double x, double y){
 	return x+y;
 }
@@ -14,5 +15,18 @@ double mult(double x, double y){
 }
 
 int main (){
-
+	int x,y;
+	char sinal;
+	printf("Digite 'x'(operação)'y':\n");
+	scanf("%d %c %d",&x, sinal, &y);
+	if(sinal == "+"){
+		printf ("%lf"soma(x,y));
+	} else if(sinal == "-"){
+		printf ("%lf"sub(x,y));
+	} else if(sinal == "/"){
+		printf ("%lf"div(x,y));
+	} else if(sinal == "*"){
+		printf ("%lf"mult(x,y));
+	}
+	
 }
